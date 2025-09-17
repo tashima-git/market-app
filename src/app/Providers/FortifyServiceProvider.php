7b811de5d01042830d68e394cdb51e006ee2f69f
tashima <shoggoth80@gmail.com>
2025-09-17 17:45:34 +0900

    初回コミット

---
+++ src/app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Fortify::registerView(fn() => view('auth.register'));
+        Fortify::loginView(fn() => view('auth.login'));
+    }
+}
